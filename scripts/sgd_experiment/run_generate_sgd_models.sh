e169aa4a47ee96731f0997b13d8b1f5341582bbf
martindunsche <martin.dunsche@rub.de>
2025-05-22 13:53:29 +0200

    Update notebooks and figures in paper

--- scripts/sgd_experiment/run_generate_sgd_models.sh
+++ scripts/sgd_experiment/run_generate_sgd_models.sh
@@ -2,13 +2,13 @@
 
 # Default values
 NUM_WORKERS=64
-INTERNAL_RESULT_PATH="/scratch/bell/wei402/fdp-estimation/results"
+INTERNAL_RESULT_PATH="/home/snek/martind/fdp/revision/rrr"
 MODEL_NAME="convnet_balanced"
 NUM_TRAIN_SAMPLES=1000
 NUM_TEST_SAMPLES=1000
 DATABASE_SIZE=1000
-EPOCHS=20
-INTERMEDIATE_EPOCH_LIST="[1, 5, 10, 15, 20]"
+EPOCHS=25
+INTERMEDIATE_EPOCH_LIST="[1, 5, 10, 15, 25]"
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -63,7 +63,7 @@ echo "Model name: $MODEL_NAME"
 echo "Intermediate epochs: $INTERMEDIATE_EPOCH_LIST"
 # Run the script
 
-python scripts/sgd_experiment/generate_sgd_models.py \
+python3 scripts/sgd_experiment/generate_sgd_models.py \
         --num_train_samples $NUM_TRAIN_SAMPLES \
         --num_test_samples $NUM_TEST_SAMPLES \
         --database_size $DATABASE_SIZE \
